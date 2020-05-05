fce250f4198ae3ee67648d07e69c48f9b210b345
Adam Jones <adam@linuxtampa.com>
2020-05-04 23:12:28 -0400

    Made some major CSS changes

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -10,6 +10,7 @@ const CoinInfo = props => {
       `https://api.allorigins.win/get?url=https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=76b58b26-5c6a-46d7-84e9-de18c7e824bd`
     )
     const json = JSON.parse(resp.data.contents)
+    console.log(json)
     setInfo(json)
   }
 
@@ -20,23 +21,32 @@ const CoinInfo = props => {
   return (
     <div className="coins" key="coin">
       <h3>Cryptocurrency Information</h3>
-      {info.data.map(coin => {
-        const round = coin.quote.USD.price.toFixed(2)
-        const roundper = coin.quote.USD.percent_change_24h.toFixed(2)
-        return (
-          <div className="center-align card-panel hoverable chip" key={coin.name}>
-            <span>{coin.name}</span>
-            <span className="coin-name">
-              {coin.symbol} {':'} &nbsp;
-            </span>
-            <span className="price">
-              {'$'}
-              {round} &nbsp;
-            </span>
-            <span className="percent">{roundper}%</span>
-          </div>
-        )
-      })}
+      <div className="content">
+        {info.data.map((coin, idx) => {
+          const round = coin.quote.USD.price.toFixed(2)
+          const roundper = coin.quote.USD.percent_change_24h.toFixed(2)
+          return (
+            <div className="col xs12 m6" key={idx}>
+              <div className="card darken-1">
+                <div className="card-content white-text">
+                  <span>Coin Name: {coin.name}</span>
+                  <span className="coin-name">
+                    Coin Symbol: {coin.symbol}
+                  </span>
+                  <span className="price">
+                    Price per unit: {'$'}
+                    {round}
+                  </span>
+                  <span className="percent">24 Hours percentage change: {roundper}%</span>
+                </div>
+                <div className="card-action">
+                  <a href="#">More Info</a>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div >
   )
 }
